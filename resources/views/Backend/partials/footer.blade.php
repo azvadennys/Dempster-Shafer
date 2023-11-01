deadeb6b46b7cf97e7ff7168222e3d8587be8b3c
Azvadennys Vasiguhamiaz <azvadenis@gmail.com>
2023-11-01 15:06:00 +0700

    first commit

---
+++ resources/views/Backend/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-custom-one">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto text-custom-600">
+            <span>Copyright &copy; Your Website 2023</span>
+        </div>
+    </div>
+</footer>
