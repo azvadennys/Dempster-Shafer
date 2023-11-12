40735c69adc7e4e9b2b3f7339f8ea1efe9d8f574
Azvadennys Vasiguhamiaz <azvadenis@gmail.com>
2023-11-13 01:48:46 +0700

    Update Icon

--- resources/views/Backend/layouts/main.blade.php
+++ resources/views/Backend/layouts/main.blade.php
@@ -20,6 +20,7 @@
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
+    <link rel="icon" href="{{ asset('8576739-01.png') }}">
     <!-- Datatables -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.2.8/css/rowReorder.dataTables.min.css">
@@ -129,7 +130,7 @@
             });
         });
     </script>
-      @stack('custome_js')
+    @stack('custome_js')
     <script src="{{ URL::to('bin/js/datapenyakit.js') }}"></script>
 
 </body>
